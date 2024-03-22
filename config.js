85ab70f96f08276fe0753e9671c82ed7abf0250c
marcescala <marcescala@yahoo.com>
2024-03-22 17:30:58 -0500

    deploy y seguridad

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.0.1:27017/burger_queen';
-exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
-exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
+require('dotenv').config();
+
+exports.port = process.argv[2] || process.env.PORT;
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL;
+exports.secret = process.env.JWT_SECRET;
+exports.adminEmail = process.env.ADMIN_EMAIL;
+exports.adminPassword = process.env.ADMIN_PASSWORD;
